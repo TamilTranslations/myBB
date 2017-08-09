2f12a35b3ea25e1869cba9ed39786697d01ff3a7
Sivaguhan S <ssivaguhan@gmail.com>
2017-08-09 06:39:59 -0400

    completed announcement.lang.php

--- inc/languages/tamil/announcements.lang.php
+++ inc/languages/tamil/announcements.lang.php
@@ -16,7 +16,7 @@
 // Language pack translators website : https://surangam.net/
 // Compatible version of MyBB : 1812
 // Last edited in MyBB Editor by : Surangam
-// Last edited date : Wed, 09 Aug 2017 10:37:13 +0000
+// Last edited date : Wed, 09 Aug 2017 10:39:41 +0000
 // --------------------------------------------------------------------------------
 
 $l['nav_announcements'] = 'மன்ற அறிவிப்பு';
@@ -25,4 +25,4 @@ $l['forum_announcement'] = 'மன்ற அறிவிப்பு: {1}';
 $l['error_invalidannouncement'] = 'குறிப்பிட்ட அறிவிப்பு செல்லுபடியாகாதது';
 $l['announcement_edit'] = 'இந்த அறிவிப்பை திருத்து';
 $l['announcement_qdelete'] = 'இந்த அறிவிப்பை நீக்கு';
-$l['announcement_quickdelete_confirm'] = 'இந்த அறிவிப்பை நீக்க கட்டாயம்  வேண்டுகிறீர்களா??';
+$l['announcement_quickdelete_confirm'] = 'இந்த அறிவிப்பை நீக்க கட்டாயம்  வேண்டுகிறீர்களா?';
