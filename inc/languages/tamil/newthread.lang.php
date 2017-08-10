a46373f81924327fb309d6910ef9da4d8c6c051f
Sivaguhan S <ssivaguhan@gmail.com>
2017-08-10 00:23:41 -0400

    completed newthread.php entries

--- inc/languages/tamil/newthread.lang.php
+++ inc/languages/tamil/newthread.lang.php
@@ -16,7 +16,7 @@
 // Language pack translators website : https://surangam.net/
 // Compatible version of MyBB : 1812
 // Last edited in MyBB Editor by : Surangam
-// Last edited date : Wed, 09 Aug 2017 11:50:17 +0000
+// Last edited date : Thu, 10 Aug 2017 04:22:37 +0000
 // --------------------------------------------------------------------------------
 
 $l['nav_newthread'] = 'புதிய இழை';
@@ -26,36 +26,38 @@ $l['thread_subject'] = 'இழையின் பொருள்';
 $l['your_message'] = 'உங்கள் விடுகை';
 $l['post_options'] = 'இடுகை தெரிவுகள்:';
 $l['options_sig'] = '<b>கையொப்பம்:</b> உங்களுது கையொப்பத்தை உள்ளடக்கு.(பெயர்ப்பதிந்த பயனர்கள் மட்டும்)';
-$l['options_emailnotify'] = '';
-$l['options_disablesmilies'] = '';
+$l['options_emailnotify'] = '<b>மின்னஞ்சல் அறிவிக்கை:</b> ஒரு புதிய பதில் வரும் போதெல்லாம் ஒரு மின்னஞ்சலை பெற்றிடு. (பெயர்ப்பதிந்த பயனர்களுக்கு மட்டும்)';
+$l['options_disablesmilies'] = '<b>உணர்ச்சிச்சித்திரங்களை முடக்கு:</b> இந்த இடுகையில் உணர்ச்சிச்சித்திரங்களை காண்பிப்பதில் இருந்து முடக்கு.';
 $l['post_thread'] = 'இழையை பதிவிடு';
 $l['preview_post'] = 'இடுகையை முன்னோட்டமிடு';
 $l['poll'] = 'வாக்கெடுப்பு:';
-$l['poll_desc'] = '';
+$l['poll_desc'] = 'விருப்பமிருந்தால் நீங்கள் இந்த இலையுடன் ஒரு வாக்கெடுப்பை இணைக்கலாம்';
 $l['poll_check'] = 'நான் ஒரு வாக்கெடுப்பை பதிவிட விரும்புகிறேன்';
 $l['num_options'] = 'தெரிவுகள் எண்ணிக்கை:';
 $l['max_options'] = '(அதிகபட்சம்: {1})';
 $l['mod_options'] = 'நெறியாளர் தெரிவுகள்';
-$l['close_thread'] = '';
-$l['stick_thread'] = '';
-$l['draft_saved'] = '';
-$l['error_post_already_submitted'] = '';
+$l['close_thread'] = '<b>இழியினை மூடிவிடு</b>: இந்த இழையில் மேலும் பதிவிடுவதில் இருந்து தடுத்துவிடும்.';
+$l['stick_thread'] = '<b>இழையினை ஒட்டிவிடு:</b> மன்றத்தின் உச்சியில் இந்த இழையினை ஒட்டிவிடு.';
+$l['draft_saved'] = 'இந்த புதியஸ் இழை வரைவாக வெற்றிகரமாக சேமிக்கப்பது.<br />நீங்கள் உங்களது வரைவு பட்டியலுக்கு அழைத்து செல்லப்படுவீர்கள்.';
+$l['error_post_already_submitted'] = 'நீங்கள் இந்த மன்றத்தில் இந்த இழையை ஏற்கெனவே பதிந்துவிட்டீர்கள். உங்களுடைய இழையை நீங்கள் காண தயவுசெய்து மன்றத்திற்கு செல்லவும்.';
 $l['no_prefix'] = 'முன்னொட்டு இல்லை';
 $l['forum_rules'] = '{1} - விதிமுறைகள்';
-$l['multiquote_external_one'] = '';
-$l['multiquote_external'] = '';
+$l['multiquote_external_one'] = 'நீங்கள் வேறொரு இழையில் இருந்து ஒரு இடுகையை தேர்ந்தெடுத்துள்ளீர்கள்.';
+$l['multiquote_external'] = 'நீங்கள் வேறு இழைகளில் இருந்து {1} இடுகைகளை தேர்ந்தெடுத்துள்ளீர்கள்.';
 $l['multiquote_external_one_deselect'] = 'இந்த இடுகையை தேர்வுநீக்கு';
 $l['multiquote_external_deselect'] = 'இந்த இடுகைகளை தேர்வுநீக்கு';
 $l['multiquote_external_one_quote'] = 'இந்த இடுகையையும் மேற்கோளிடு';
 $l['multiquote_external_quote'] = 'இந்த இடுகைகளையும் மேற்கோளிடு';
-$l['redirect_newthread'] = '';
-$l['redirect_newthread_poll'] = '';
-$l['redirect_newthread_moderation'] = '';
-$l['redirect_newthread_unviewable'] = '';
-$l['redirect_newthread_thread'] = '';
-$l['invalidthread'] = '';
-$l['error_stop_forum_spam_spammer'] = '';
-$l['error_stop_forum_spam_fetching'] = '';
-$l['error_suspendedposting'] = '';
-$l['error_suspendedposting_temporal'] = '';
-$l['error_suspendedposting_permanent'] = '';
+$l['redirect_newthread'] = 'நன்றி, உங்களது இழை பதிக்கப்பட்டது.';
+$l['redirect_newthread_poll'] = '<br />நீங்கள் தற்போது வாக்கெடுப்பு தெரிவுகள் மற்றும் வடிவமைப்பு பக்கத்திற்கு அழைத்து செல்லப்படுவீர்கள்.';
+$l['redirect_newthread_moderation'] = '<br />நிர்வாகி அனைத்து புதிய இழைகளையும் மட்டுறுத்தலுக்கு உட்படுத்தும்படி குறிப்பிட்டுள்ளார். நீங்கள் இப்போது இழைகளின் பட்டியலுக்கு திருப்ப அனுப்பப்படுவீர்கள்.';
+$l['redirect_newthread_unviewable'] = '<br /> உங்களுக்கு இந்த மன்றத்தில் உள்ள இழைகளை காண அனுமதியில்லை. நீங்கள் அந்த மன்றத்திற்கு திருப்பி அனுப்பப்படுவீர்கள்.';
+$l['redirect_newthread_thread'] = '<br />நெனெகல் இந்த புதிய இழைக்கு அழைத்து செல்லப்படுவீர்கள்';
+$l['invalidthread'] = 'குறிப்பிட்ட வரைவு இருக்கவில்லை அல்லது நீங்கள் அதை காண உங்களுக்கு அனுமதியில்லை.';
+$l['error_stop_forum_spam_spammer'] = ' மன்னிக்கவும்,உங்களது {1} ஒரு தெரிந்த எரித பரப்பாளரினை ஒத்துள்ளது. நீங்கள் இது ஒரு தவறென்று உணர்ந்தாள், ஒரு நிர்வாகியை தொடர்பு கொள்ளவும்.';
+$l['error_stop_forum_spam_fetching'] = 'மன்னிக்கவும், உங்களது இழையை ஒரு எரித்த பரப்பாளர் தரவுத்தளத்தில் சரிப்பார்ப்பதில் ஏதோ தவறு ஏற்பட்டுவிட்டது. பெரும்பாலும் தரவுத்தளத்தினை அணுக முடியாமல் இருக்கலாம்.';
+$l['error_suspendedposting'] = 'உங்களது பதிவிடும் சலுகைகள் இடைநீக்கம் {1} செய்யப்பட்டுள்ளது .<br /><br />
+
+இடைநீக்கம் செய்யப்பட்ட நாள் : {2} ';
+$l['error_suspendedposting_temporal'] = '{1} வரை';
+$l['error_suspendedposting_permanent'] = 'நிரந்திரமாக';
